4b80ff1625eb104e63c65352d77786bc6c15b313
reddflower <bokhandanil09@gmail.com>
2026-09-05 21:41:20 +0300

    Update autoloader.bat

--- autoloader.bat
+++ autoloader.bat
@@ -1,4 +1,7 @@
 @echo off
+set "LOCAL_VERSION=1.0"
+set "GITHUB_VERSION_URL=https://reddflower.github.io/autoloader_version.html"
+set "GITHUB_DOWNLOAD_URL=https://github.com/reddflower/zapret_autoloader/releases/latest"
 chcp 437 > nul
 setlocal enableDelayedExpansion
 
@@ -16,12 +19,15 @@ if "%no-zapret%"=="0" (
     exit
 )
 
-:: COPY =========================================================
+echo Patch: replace general*.bat...
+powershell -NoProfile -Command "$file='service.bat'; $old='Get-ChildItem -LiteralPath ''.'' -Filter ''*.bat'' | Where-Object { $_.Name -notlike ''service*'' } | Sort-Object { [Regex]::Replace($_.Name, ''(\d+)'', { $args[0].Value.PadLeft(8, ''0'') }) } | ForEach-Object { $_.Name }'; $new='Get-ChildItem -LiteralPath ''.'' -Filter ''general*.bat'' | Sort-Object { [Regex]::Replace($_.Name, ''(\d+)'', { $args[0].Value.PadLeft(8, ''0'') }) } | ForEach-Object { $_.Name }'; $content=Get-Content $file -Raw; $content=$content.Replace($old, $new); Set-Content $file -Value $content -Encoding OEM"
 
-if exist "generals_copy\" echo.
-md generals_copy
-copy "general*.bat" "./generals_copy" 
-cls
+if errorlevel 1 (
+    call :PrintRed "Unknown error"
+
+) else (
+    goto menu
+)
 
 :: MENU ===========================================================
 
@@ -29,18 +35,28 @@ cls
 cls
 set "menu_choice=null"
 
+:: Get the latest version from GitHub
+for /f "delims=" %%A in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri \"%GITHUB_VERSION_URL%\" -Headers @{\"Cache-Control\"=\"no-cache\"} -UseBasicParsing -TimeoutSec 5).Content.Trim()" 2^>nul') do set "GITHUB_VERSION=%%A"
+
 echo.
-echo   ZAPRET AUTOLOADER
+if "%LOCAL_VERSION%"=="%GITHUB_VERSION%" (
+echo      ZAPRET AUTOLOADER v!LOCAL_VERSION!
+) else ( 
+set "new_version=avaible"
+echo      ZAPRET AUTOLOADER v!LOCAL_VERSION!
+call :PrintGreen "     NEW VERSION AVAIBLE: %GITHUB_VERSION% "
+)
 echo   ----------------------------------------
 echo.
 echo      1. Install AutoLoader
 echo      2. Uninstall AutoLoader
 echo      3. Check Status
 echo.
-echo      4. Minecraft FIX
+echo      4. Open Zapret Service
 echo.
-echo      5. Open Zapret Service
+echo      5. Minecraft fix (25565 port)
 echo      0. Exit
+if "%new_version%"=="avaible" call :PrintGreen "     upd. For update"
 echo.
 echo   ----------------------------------------
 echo.
@@ -50,18 +66,44 @@ set /p menu_choice=   Select option (1-5):
 if "%menu_choice%"=="1" goto service_updater
 if "%menu_choice%"=="2" goto service_uninstall
 if "%menu_choice%"=="3" goto check_status
-if "%menu_choice%"=="4" goto minecraft_fix
-if "%menu_choice%"=="5" goto zapret_menu
+if "%menu_choice%"=="4" goto zapret_menu
+if "%menu_choice%"=="5" goto minecraft_fix
 if "%menu_choice%"=="0" exit /b
+if "%menu_choice%"=="upd" start "" "%GITHUB_DOWNLOAD_URL%"
+goto menu
 
 :: Minecraft fix ========================================================
 
 :minecraft_fix
 cls
 
+echo The following files will be updated (minecraft fix 25565):
 echo.
-echo Coming soon...:
+set "count=0"
+
+for /f "delims=" %%F in ('powershell -NoProfile -Command "Get-ChildItem -LiteralPath '.' -Filter 'general*.bat' | Where-Object { $_.Name -notlike 'service*' } | Where-Object { $_.Name -notlike 'hello_world*' } | Sort-Object { [Regex]::Replace($_.Name, '(\d+)', { $args[0].Value.PadLeft(8, '0') }) } | ForEach-Object { $_.Name }"') do (
+    set /a count+=1
+    echo !count!. %%F
+)
 echo.
+
+if !count! equ 0 (
+    echo.
+    call :PrintRed "Generals not found"
+    echo.
+    pause
+    goto menu
+)
+
+pause
+
+echo Install 25565 port and script...
+echo.
+
+for %%i in (general*.bat) do (
+    echo Processing: %%i
+    powershell -Command "$file='%%i'; $content=Get-Content $file -Raw; $already=$content -match '--filter-tcp=25565'; if (-not $already) { $content=$content -replace '--wf-tcp=80,443,2053,2083,2087,2096,8443,%%GameFilterTCP%%', '--wf-tcp=80,443,2053,2083,2087,2096,8443,25565,%%GameFilterTCP%%'; $content=$content -replace '--dpi-desync-cutoff=n([2-4])\s*$', ('--dpi-desync-cutoff=n$1 --new ^' + [Environment]::NewLine + '--filter-tcp=25565 --ipset-exclude=\"' + [char]37 + 'LISTS' + [char]37 + 'ipset-exclude.txt\" --dpi-desync-any-protocol=1 --dpi-desync-cutoff=n5 --dpi-desync=multisplit --dpi-desync-split-seqovl=582 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern=\"' + [char]37 + 'BIN' + [char]37 + 'tls_clienthello_4pda_to.bin\"'); Set-Content $file -Value $content -Encoding OEM; Write-Host 'Applied' -ForegroundColor Green } else { Write-Host 'Already patched, skipped' -ForegroundColor Red }"
+)
 
 pause
 goto menu
@@ -75,7 +117,6 @@ echo.
 echo Starting Zapret...:
 start service.bat
 
-pause
 goto menu
 
 :: CHECK STATUS ========================================================
@@ -127,8 +168,6 @@ goto menu
 :service_updater
 cls
 
-
-
 echo.
 echo The following files will be updated and sent to backups (generals_copy):
 set "count=0"
@@ -146,12 +185,17 @@ pause
 goto menu
 )
 
+if exist "generals_copy\" echo.
+md generals_copy
+copy "general*.bat" "./generals_copy" 
+cls
+
 pause
 
 echo.
 for %%i in (general*.bat) do (
     echo Processing: %%i
-    powershell -Command "& { (Get-Content '%%i') | ForEach-Object { $_.Replace('--hostlist=\"%%LISTS%%list-general.txt\"', '--hostlist-auto=\"%%LISTS%%list-general.txt\"') } | Set-Content '%%i' }"
+    powershell -Command "& { (Get-Content '%%i') | ForEach-Object { $_.Replace('--hostlist=\"%%LISTS%%list-general-user.txt\"', '--hostlist-auto=\"%%LISTS%%list-general-user.txt\"') } | Set-Content '%%i' }"
 )
 
 pause
@@ -188,7 +232,7 @@ pause
 echo.
 for %%i in (general*.bat) do (
     echo Processing: %%i
-    powershell -Command "& { (Get-Content '%%i') | ForEach-Object { $_.Replace('--hostlist-auto=\"%%LISTS%%list-general.txt\"', '--hostlist=\"%%LISTS%%list-general.txt\"') } | Set-Content '%%i' }"
+    powershell -Command "& { (Get-Content '%%i') | ForEach-Object { $_.Replace('--hostlist-auto=\"%%LISTS%%list-general-user.txt\"', '--hostlist=\"%%LISTS%%list-general-user.txt\"') } | Set-Content '%%i' }"
 )
 
 pause
@@ -208,3 +252,8 @@ exit /b
 :PrintYellow
 powershell -NoProfile -Command "Write-Host \"%~1\" -ForegroundColor Yellow"
 exit /b
+
+
+
+
+:: "winws.exe" ^ DONT. INSTALL. AUTOLOADER. FROM. INSTALL SERVICE. FROM SERVICE.BAT. run autoloader.bat yourself, dolbaeb
